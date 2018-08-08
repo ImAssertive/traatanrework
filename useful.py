a01d7da961120e5488ee2ea52985b96d70b8dade
ImAssertive <imassertive@gmail.com>
2018-08-08 09:36:16 +0100

    further exec testing

--- useful.py
+++ useful.py
@@ -52,11 +52,7 @@ async def menuFunction(ctx, titleText, options, footerText=None, timeoutTime = 6
         else:
             for k in range(0,len(options)):
                 if msg.content.lower() in options[k][1]:
-                    exec("ctx.bot.cogs['adminCog'].test(ctx)")
-                    await ctx.bot.cogs['adminCog'].test1(ctx)
-                    print([x for x, y in ctx.bot.cogs['adminCog'].__dict__.items() if type(y) == FunctionType])
-                    func = getattr(ctx.bot.cogs['adminCog'], str([options[k][0][1]]))
-                    func(ctx)
+                    exec(str([options[k][0][1]]))
 
             print("This should probably never be seen.")
         print("This should probably never be seen number 2.")
