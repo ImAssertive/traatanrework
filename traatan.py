b27957541b1988d5e1616266da7b1e9cbd58d6ab
ImAssertive <imassertive@gmail.com>
2018-08-10 22:14:36 +0100

    more fixes

--- traatan.py
+++ traatan.py
@@ -141,11 +141,11 @@ class Bot(commands.Bot):
         await self.change_presence(status=discord.Status.online, activity=game)
         self.prefixes = []
         query = "SELECT * FROM Guilds WHERE prefix IS NOT NULL"
-        results = await self.db.fetch(query, ctx.guild.id)
+        results = await self.db.fetch(query)
         for result in results:
             self.prefixes.append([result["guildid"], result["prefix"]])
         query = "SELECT * FROM Users WHERE prefix IS NOT NULL"
-        results = await self.db.fetchrow(query, ctx.author.id)
+        results = await self.db.fetchrow(query)
         for result in results:
             self.prefixes.append([result["userid"], result["prefix"]])
 
